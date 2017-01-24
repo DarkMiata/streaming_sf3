8692572fda54e4a10ef388c5d60a1318ff25a47e
DarkMiata <samvermeulen@gmail.com>
2017-01-24 12:30:45 +0100

    Avant ORM - jointure bis ajout de la classe Pays

---
+++ src/AppBundle/Entity/Pays.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Pays
+ *
+ * @ORM\Table(name="pays")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\PaysRepository")
+ */
+class Pays
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
+
